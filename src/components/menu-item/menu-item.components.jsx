a1f400df3c375b4873f0d4bb090a829e760b3fe5
Kelvin Mihingo <kmihingo22@gmail.com>
2021-01-20 09:56:33 +0300

    Background transition effect

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './menu-items.styles.scss';
 
-const MenuItem = ({title, imageUrl}) => (
-    <div style={
-        {
-            backgroundImage: `url(${imageUrl})`
-        }
-    } className='menu-item'>
+const MenuItem = ({title, imageUrl, size}) => (
+    <div className={`${size} menu-item`}
+    >
+        <div className='background-image'
+            style={
+                {
+                    backgroundImage: `url(${imageUrl})`
+                }}
+        />
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>Shop Now!</span>
